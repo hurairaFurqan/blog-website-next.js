872c7bb4c501b387b85e86023cbe5538f3c0db7f
hurairaFurqan <hurairafurqan13@gmail.com>
2024-02-20 14:47:06 +1100

    basic structure

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+export default function Home() {
+  return <div>
+    <Link href="/">
+      
+      <h1>LoremProident</h1>
+      LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.
+      LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.LoremProident velit nisi duis est amet sint minim ex veniam. Dolor occaecat consequat adipisicing fugiat pariatur
+      ullamco commodo minim deserunt irure dolore. Tempor cillum incididunt nisi exercitation et proident sint et. Magna cupidatat
+      officia ad exercitation commodo.</Link>
+  </div>
+}
