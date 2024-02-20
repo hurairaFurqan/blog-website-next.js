b08c5b070e8e9d8775106e02d5dcbb6ccd29ea99
hurairaFurqan <hurairafurqan13@gmail.com>
2024-02-20 16:02:21 +1100

    in progress navbar Design

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./AuthLinks.module.css"
+const AuthLinks = () => {
+    return <div className={styles.container}>AuthLinks</div>
+}
+export default AuthLinks
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./themetoggle.module.css"
+const ThemeToggle = () => {
+    return <div className={styles.container}>ThemeToggle</div>
+}
+export default ThemeToggle
